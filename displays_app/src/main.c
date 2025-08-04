97eda32b3652f50c5d6275e18997e55e39a47e8f
Marcio Ribeiro <wmrsouza@hotmail.com>
2025-08-04 00:46:11 -0300

    cfb: baseline
    Signed-off-by: Marcio Ribeiro <wmrsouza@hotmail.com>

---
+++ displays_app/src/main.c
@@ -0,0 +1,12 @@
+#include <app_version.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+int main(void) {
+  LOG_INF("Display sample apllication");
+
+  return 0;
+}
